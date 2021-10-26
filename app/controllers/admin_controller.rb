d83a9efe17bbc1551703a55c0ac63ac534683364
Micha Bernhard <mib70@pitt.edu>
2021-10-25 21:25:03 -0600

    Change names of pets to make use of many to many

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,4 +3,8 @@ class AdminController < ApplicationController
     @shelters = Shelter.find_by_sql("SELECT * FROM shelters ORDER BY shelters.name DESC")
     @pending_pets = Pet.pending_shelters
   end
+
+  def show
+    @application = Application.find(params[:id])
+  end
 end 
