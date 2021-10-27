1fe97d11580d8252b908d377da6dd77b79d60dbb
Micha Bernhard <mib70@pitt.edu>
2021-10-27 15:58:07 -0600

    Fix within id in test

--- spec/features/admin/admin_applications_show_spec.rb
+++ spec/features/admin/admin_applications_show_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'Admin Applications Show page' do
     shelter1 = Shelter.create!(name: 'Awesome Shelter', city: 'Pittsburgh PA', foster_program: false, rank: 9)
     pet1 = Pet.create!(name: 'Charles', age: 2, breed: 'Doberman', adoptable: true, shelter_id: shelter1.id)
     pet2 = Pet.create!(name: 'Kevin', age: 2, breed: 'Doberman', adoptable: true, shelter_id: shelter1.id)
-    app1 = Application.create!(name: 'Jen', street_address: '123 Street Dr', city: 'Pittsburgh', state: 'PA', zip_code: '15238', description: "I really like dogs", names_of_pets: "#{pet1.name} #{pet2.name}", status: 'Pending')
+    app1 = Application.create!(name: 'Jen', street_address: '123 Street Dr', city: 'Pittsburgh', state: 'PA', zip_code: '15238', description: "I really like dogs", status: 'Pending')
 
     PetApplication.create!(pet: pet1, application: app1)
 
@@ -15,18 +15,18 @@ RSpec.describe 'Admin Applications Show page' do
     expect(page).to have_button("Approve")
   end
 
-  xit 'gets rid of the button and labels the pet name as approved if clicked' do
+  it 'gets rid of the button and labels the pet name as approved if clicked' do
     shelter1 = Shelter.create!(name: 'Awesome Shelter', city: 'Pittsburgh PA', foster_program: false, rank: 9)
     pet1 = Pet.create!(name: 'Charles', age: 2, breed: 'Doberman', adoptable: true, shelter_id: shelter1.id)
     pet2 = Pet.create!(name: 'Kevin', age: 2, breed: 'Doberman', adoptable: true, shelter_id: shelter1.id)
-    app1 = Application.create!(name: 'Jen', street_address: '123 Street Dr', city: 'Pittsburgh', state: 'PA', zip_code: '15238', description: "I really like dogs", names_of_pets: "#{pet1.name} #{pet2.name}", status: 'Pending')
+    app1 = Application.create!(name: 'Jen', street_address: '123 Street Dr', city: 'Pittsburgh', state: 'PA', zip_code: '15238', description: "I really like dogs", status: 'Pending')
 
     PetApplication.create!(pet: pet1, application: app1)
     PetApplication.create!(pet: pet2, application: app1)
 
     visit "admin/applications/#{app1.id}"
 
-    within("id-#{pet1.id}") do
+    within("#id-#{pet1.id}") do
       click_button 'Approve'
       expect(page).to have_content('Charles: Approved')
     end
@@ -36,7 +36,7 @@ RSpec.describe 'Admin Applications Show page' do
     shelter1 = Shelter.create!(name: 'Awesome Shelter', city: 'Pittsburgh PA', foster_program: false, rank: 9)
     pet1 = Pet.create!(name: 'Charles', age: 2, breed: 'Doberman', adoptable: true, shelter_id: shelter1.id)
     pet2 = Pet.create!(name: 'Kevin', age: 2, breed: 'Doberman', adoptable: true, shelter_id: shelter1.id)
-    app1 = Application.create!(name: 'Jen', street_address: '123 Street Dr', city: 'Pittsburgh', state: 'PA', zip_code: '15238', description: "I really like dogs", names_of_pets: "#{pet1.name} #{pet2.name}", status: 'Pending')
+    app1 = Application.create!(name: 'Jen', street_address: '123 Street Dr', city: 'Pittsburgh', state: 'PA', zip_code: '15238', description: "I really like dogs", status: 'Pending')
 
     PetApplication.create!(pet: pet1, application: app1)
 
